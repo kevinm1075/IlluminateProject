5b06cc6aacc7367a44f70448431c1516e221aa22
Kevin Misir <kevinm1075@gmail.com>
2015-11-10 21:14:40 -0500

    UI Improvements

--- LightsOutProject/App_Start/RouteConfig.cs
+++ LightsOutProject/App_Start/RouteConfig.cs
@@ -14,13 +14,13 @@ namespace LightsOutProject
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
             routes.IgnoreRoute("{*x}", new { x = @".*\.asmx(/.*)?" });
    
-   /*         
+            /*         
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
-                
-            );*/
+                );
+            */
             
         }
     }
